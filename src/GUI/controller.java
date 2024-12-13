f0df202d31dce276304ce0b075ca81215fac5e4b
Ramesh Edirisinghe <152771280+RameshEdirisinghe@users.noreply.github.com>
2024-12-13 21:23:04 +0530

    Update controller.java

--- src/GUI/controller.java
+++ src/GUI/controller.java
@@ -2,54 +2,91 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
-package GUI;
+package Main;
 
-public class controller implements controllerObserverInterface {
+import Main.Helicopter;
+import Main.Helicopter;
+import Main.Submarine;
+import Main.Submarine;
+import Main.Tank;
+import Main.Tank;
+import javaInterfaces.controllerObserver;
+import javaInterfaces.controllerObserverInterface;
+
+public class Observerble implements controllerObserverInterface {
 
     private controllerObserver[] con;
     private int position;
     private Helicopter heli;
     private Tank warTank;
     private Submarine sub;
+    private boolean Data;
 
-    controller(Helicopter heli,Tank warTank,Submarine sub) {
+    Observerble() {
         con = new controllerObserver[0];
-        this.heli=heli;
-        this.warTank=warTank;
-        this.sub=sub;
+
     }
 
-//    private void extendsObserverArray() {
-//        controllerObserver[] tempcon = new controllerObserver[con.length + 1];
-//        for (int i = 0; i < con.length; i++) {
-//            tempcon[i] = con[i];
-//        }
-//        con = tempcon;
-//    }
-//
-//    public void addControlObserver(controllerObserver vehicles) {
-//        extendsObserverArray();
-//        con[con.length - 1] = vehicles;
-//    }
+    public void setVehicles(Helicopter heli, Tank warTank, Submarine sub) {
+        this.heli = heli;
+        this.warTank = warTank;
+        this.sub = sub;
+    }
 
-    public void setSliderLevel(int position,String vehicleType) {
+    private void extendsObserverArray() {
+        controllerObserver[] tempcon = new controllerObserver[con.length + 1];
+        for (int i = 0; i < con.length; i++) {
+            tempcon[i] = con[i];
+        }
+        con = tempcon;
+    }
+
+    public void addControlObserver(controllerObserver vehicles) {
+        extendsObserverArray();
+        con[con.length - 1] = vehicles;
+    }
+
+    public void setSliderLevel(int position, String vehicleType) {
         if (this.position != position) {
             this.position = position;
         }
         notifyObjects(vehicleType);
     }
 
-    public void notifyObjects(String vehicleType) {
-     
-            if (vehicleType.equals("Helicopter")) {
-                heli.update(position);
-                
-            }else if(vehicleType.equals("Tank")){
-                warTank.update(position);
-            }else if(vehicleType.equals("Submarine")){
-                sub.update(position);
-            }
-            
+    public void setData(boolean Data) {
         
+        if (this.Data != Data) {
+            this.Data = Data;
+        }
+        passData();
+    }
+
+    public void msgAdminToAll(String msg) {
+        for (controllerObserver ob : con) {
+            System.out.println(msg);
+            ob.receivedMsg(msg);
+        }
+
+    }
+
+    public void passData() {
+
+        for (controllerObserver ob : con) {
+            System.out.println(Data);
+            ob.updateData(Data);
+        }
+
+    }
+
+    public void notifyObjects(String vehicleType) {
+
+        if (vehicleType.equals("Helicopter")) {
+            heli.update(position);
+        } else if (vehicleType.equals("Tank")) {
+            warTank.update(position);
+        } else if (vehicleType.equals("Submarine")) {
+            sub.update(position);
+        }
+
     }
 }
