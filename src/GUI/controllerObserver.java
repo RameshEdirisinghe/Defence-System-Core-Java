22a3f99036fef680a9f07fc9ebd613dee3f6304f
Ramesh Edirisinghe <152771280+RameshEdirisinghe@users.noreply.github.com>
2024-12-13 21:22:14 +0530

    Update controllerObserver.java

--- src/GUI/controllerObserver.java
+++ src/GUI/controllerObserver.java
@@ -2,9 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package GUI;
+package javaInterfaces;
 
 public interface controllerObserver{
     public void update(int positionLevel);
+    public void updateData(boolean data);
+    public void receivedMsg(String rMsg);
     
 }
