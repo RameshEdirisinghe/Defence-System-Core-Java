8ae2bb3fb8f421a1faf058759a61cf145c1d81d3
Ramesh Edirisinghe <152771280+RameshEdirisinghe@users.noreply.github.com>
2024-12-13 21:36:21 +0530

    Update DefenceSystem.java

--- src/GUI/DefenceSystem.java
+++ src/GUI/DefenceSystem.java
@@ -2,27 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package GUI;
+package JavaClasses;
 
-import GUI.Helicopter;
-import GUI.mainController;
+import Main.Helicopter;
+import Main.Home;
+import Main.Welcome;
 
-/**
- *
- * @author ranga
- */
+
 public class DefenceSystem {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        new Home().setVisible(true);
+        
+        new Welcome().setVisible(true);
 
-        
-        
-        
     }
     
 }
