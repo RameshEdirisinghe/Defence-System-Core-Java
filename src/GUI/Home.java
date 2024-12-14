d086e43b7dd9fe71bad4c27c233a52912038008f
Ramesh Edirisinghe <152771280+RameshEdirisinghe@users.noreply.github.com>
2024-12-14 20:02:48 +0530

    Update Home.java

--- src/GUI/Home.java
+++ src/GUI/Home.java
@@ -24,25 +24,17 @@ public class Home extends javax.swing.JFrame {
 
  
     public Home() {
-        File mp3File = new File("C:\\Users\\ranga\\Downloads\\bg_sound.mp3");
-        if (mp3File.exists()) {
-            System.out.println("File found: " + mp3File.getAbsolutePath());
-        } else {
-            System.out.println("File not found!");
-        }
-        bgMusicPlayer = new MP3Player(mp3File);
-        bgMusicPlayer.setRepeat(true);
-        bgMusicPlayer.play();
-        initComponents();
 
         initComponents();
+
+        
 
         setLocationRelativeTo(null);
     }
 
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jLabel2 = new javax.swing.JLabel();
@@ -56,7 +48,7 @@ public class Home extends javax.swing.JFrame {
 
         jLabel2.setBackground(new java.awt.Color(88, 210, 238));
         jLabel2.setFont(new java.awt.Font("Trebuchet MS", 1, 12)); // NOI18N
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/hqk720-removebg-preview.png"))); // NOI18N
+       
         jLabel2.setText("Start Game");
         jLabel2.setMaximumSize(new java.awt.Dimension(364, 128));
         jLabel2.setMinimumSize(new java.awt.Dimension(364, 128));
@@ -69,17 +61,17 @@ public class Home extends javax.swing.JFrame {
         getContentPane().add(jLabel2);
         jLabel2.setBounds(30, 170, 230, 70);
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/call-of-duty-ghosts (3).png"))); // NOI18N
+        
         jLabel3.setText("jLabel3");
         getContentPane().add(jLabel3);
         jLabel3.setBounds(30, 50, 290, 100);
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/hq720-removebg-preview.png"))); // NOI18N
+       
         jLabel4.setText("jLabel4");
         getContentPane().add(jLabel4);
         jLabel4.setBounds(30, 260, 230, 80);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/4KWITHCC-ezgif.com-video-to-gif-converter (1).gif"))); // NOI18N
+       
         jLabel1.setText("jLabel1");
         jLabel1.setMaximumSize(new java.awt.Dimension(1922, 920));
         jLabel1.setMinimumSize(new java.awt.Dimension(1922, 920));
@@ -88,47 +80,29 @@ public class Home extends javax.swing.JFrame {
         jLabel1.setBounds(0, -150, 1000, 860);
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel2MouseClicked
-        // Home Sound
-//        File mp3File = new File("C:\\Users\\ranga\\Downloads\\gun-shots-from-a-distance-7-96391.mp3");
-//        if (mp3File.exists()) {
-//            System.out.println("File found: " + mp3File.getAbsolutePath());
-//        } else {
-//            System.out.println("File not found!");
-//        }
-//        MP3Player mp3Player = new MP3Player(mp3File);
-//        mp3Player.play();
+    private void jLabel2MouseClicked(java.awt.event.MouseEvent evt) {                                     
+        
+
         
         Observerble ob = new Observerble();
         
-        mainController mainGui = new mainController(ob);
-        mainGui.setVisible(true);
+        mainController mainGui = mainController.getInstance();
+        mainGui.setObservable(ob);        
         
-        Helicopter heli = new Helicopter(mainGui);
-        Tank warTank = new Tank(mainGui);
-        Submarine sub = new Submarine(mainGui);
-        
-        ob.setVehicles(heli,warTank,sub);
-        ob.addControlObserver(heli);
-        ob.addControlObserver(warTank);
-        ob.addControlObserver(sub);
+        ob.addControlObserver(new Submarine(ob));
+        ob.addControlObserver(new Tank(ob));
+        ob.addControlObserver(new Helicopter(ob));
 
-        
-    }//GEN-LAST:event_jLabel2MouseClicked
+    }                                    
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
 
-    }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
